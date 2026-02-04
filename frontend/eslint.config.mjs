3ec7ac1746100d30697ef9b156f38bbd3b3cf121
shayanRaza92 <shayanraza2333@gmail.com>
2026-02-04 13:31:27 +0500

    Fix build: ignore all files in eslint and revert build command

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -6,12 +6,14 @@ const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
+  // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "**/*",
   ]),
 ]);
 
